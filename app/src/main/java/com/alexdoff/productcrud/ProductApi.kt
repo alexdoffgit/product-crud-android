1581eb330b672320cc0091c2d1fe99891fa802a2
alexdoffgit <alexdoffacc@gmail.com>
2022-07-21 12:20:23 +0700

    interface for getting product

---
+++ app/src/main/java/com/alexdoff/productcrud/ProductApi.kt
@@ -0,0 +1,24 @@
+package com.alexdoff.productcrud
+
+import com.alexdoff.productcrud.dataobject.ProductBody
+import com.alexdoff.productcrud.dataobject.ProductPostRes
+import com.alexdoff.productcrud.dataobject.ProductUpdateRes
+import com.alexdoff.productcrud.dataobject.Products
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ProductApi {
+    @GET("/product")
+    suspend fun getAllProduct(): Response<Products>
+
+    @POST("/product")
+    suspend fun createProduct(@Body payload: ProductBody): Response<ProductPostRes>
+
+    @PUT("/product/{id}")
+    suspend fun updateProduct(@Path("id") id: String,
+                      @Body payload: ProductBody): Response<ProductUpdateRes>
+
+    @DELETE("/product/{id}")
+    suspend fun deleteProduct(@Path("id") id: String): Response<String>
+}
