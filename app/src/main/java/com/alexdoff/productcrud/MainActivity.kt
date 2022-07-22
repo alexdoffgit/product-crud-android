2ccfc04f35fdc0ab63fc149c209243ed3ede0cab
alexdoff <alexdoffacc@gmail.com>
2022-07-22 12:56:57 +0700

    nav with list stuff

--- app/src/main/java/com/alexdoff/productcrud/MainActivity.kt
+++ app/src/main/java/com/alexdoff/productcrud/MainActivity.kt
@@ -3,6 +3,10 @@ package com.alexdoff.productcrud
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.alexdoff.productcrud.databinding.ActivityMainBinding
 import com.alexdoff.productcrud.repository.ProductRepo
 
@@ -12,8 +16,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val nc = findNavController(R.id.nav_host_fragment)
+//        return  nc.navigateUp() || super.onSupportNavigateUp()
+//    }
 }
