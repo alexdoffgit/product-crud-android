19d26a5177c7e52e0c85a6e06f428abdb4399be1
alexdoff <alexdoffacc@gmail.com>
2022-07-22 11:30:59 +0700

    purge shit

--- app/src/main/java/com/alexdoff/productcrud/MainActivity.kt
+++ app/src/main/java/com/alexdoff/productcrud/MainActivity.kt
@@ -2,10 +2,18 @@ package com.alexdoff.productcrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.alexdoff.productcrud.databinding.ActivityMainBinding
+import com.alexdoff.productcrud.repository.ProductRepo
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/alexdoff/productcrud/RetrofitInstance.kt
+++ app/src/main/java/com/alexdoff/productcrud/RetrofitInstance.kt
@@ -2,9 +2,12 @@ package com.alexdoff.productcrud
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+//import okhttp3.lo
 
 object RetrofitInstance {
     private const val baseUrl = "https://procrud.my.id/api/"
+
+//    private val interceptor: HttpLoggingInterceptor = HttpLoggingInte
 
     val api: ProductApiInterface by lazy {
         Retrofit
