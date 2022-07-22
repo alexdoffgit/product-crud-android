901ff7c5d9a6e72bcd2b9647da102c0789ab3640
alexdoff <alexdoffacc@gmail.com>
2022-07-22 13:31:44 +0700

    add http logging

--- app/src/main/java/com/alexdoff/productcrud/RetrofitInstance.kt
+++ app/src/main/java/com/alexdoff/productcrud/RetrofitInstance.kt
@@ -1,18 +1,34 @@
 package com.alexdoff.productcrud
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-//import okhttp3.lo
 
 object RetrofitInstance {
     private const val baseUrl = "https://procrud.my.id/api/"
 
-//    private val interceptor: HttpLoggingInterceptor = HttpLoggingInte
+    private fun interceptor(): HttpLoggingInterceptor {
+        val i = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            i.setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            i.setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+
+        return i
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor())
+        .build()
 
     val api: ProductApiInterface by lazy {
         Retrofit
             .Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ProductApiInterface::class.java)
